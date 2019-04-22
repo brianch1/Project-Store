d9e63a7dd08f795677795884909dedab04763c7a
brianch1 <43443832+brianch1@users.noreply.github.com>
2019-04-21 21:48:58 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+var productName=[];
+var totalPrice=[];
+var url=[];
+$(".container").click(function() {
+    var listofImages=$(".images").val();
+    var listofProducts=$(".productname").val();
+    var listofPrice=$(".price").val();
+    productName.push(listofProducts);
+    totalPrice.push(listofPrice);
+    
+});
+var price=[]; 
+price.forEach(function (){
+    $("price:" + price);
+    
+});
